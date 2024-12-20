798244a01347e825c645933f260558d6f0a5fbd6
Thành Công <congmt.ptit@gmail.com>
2024-12-21 00:12:51 +0700

    update

--- website/views.py
+++ website/views.py
@@ -171,80 +171,82 @@ def bcgio():
 @views.route('/bao-cao-gio/<int:y>-<int:m>-<int:d>', methods=['GET', 'POST'])
 @login_required
 def bao_cao_gio(y, m, d):
+    try:
+        now = datetime.now()
+        ddate = datetime.strptime(f'{y}-{m}-{d}', '%Y-%m-%d').date()
+        time_1130 = datetime.combine(ddate, datetime.min.time()).replace(hour=11, minute=30, second=0, microsecond=0)
+        time_1700 = datetime.combine(ddate, datetime.min.time()).replace(hour=17, minute=0, second=0, microsecond=0)
 
-    now = datetime.now()
-    ddate = datetime.strptime(f'{y}-{m}-{d}', '%Y-%m-%d').date()
-    time_1130 = datetime.combine(ddate, datetime.min.time()).replace(hour=11, minute=30, second=0, microsecond=0)
-    time_1700 = datetime.combine(ddate, datetime.min.time()).replace(hour=17, minute=0, second=0, microsecond=0)
-
-    if now > time_1700:
-        render_table = 0
-    elif now > time_1130:
-        render_table = 1
-    else:
-        render_table = 2
-
-    staffs = User.query.all()
-    for staff in staffs:
-        if Hour_report.query.filter(and_( Hour_report.user_id == staff.id, or_(Hour_report.time == time_1130, Hour_report.time == time_1700))).all():
-            continue
+        if now > time_1700:
+            render_table = 0
+        elif now > time_1130:
+            render_table = 1
         else:
-            user_id = staff.id
-            new_report_1130 = Hour_report(user_id=user_id, time=time_1130)
-            db.session.add(new_report_1130)
-            new_report_1700 = Hour_report(user_id=user_id, time=time_1700)
-            db.session.add(new_report_1700)
-            db.session.commit()
+            render_table = 2
 
-    if request.method == 'POST':
-        form_id = request.form.get('form_id')
-
-        if form_id == 'form-1130':
-            user_id = request.form.get('user_id')
-            report = Hour_report.query.filter_by(user_id=user_id, time=time_1130).first()
-            if report:
-                report.cost = request.form.get(f'cost')
-                report.phoneNumber = request.form.get(f'phoneNumber')
-                report.revenue = request.form.get(f'revenue')
+        staffs = User.query.all()
+        for staff in staffs:
+            if Hour_report.query.filter(and_( Hour_report.user_id == staff.id, or_(Hour_report.time == time_1130, Hour_report.time == time_1700))).all():
+                continue
+            else:
+                user_id = staff.id
+                new_report_1130 = Hour_report(user_id=user_id, time=time_1130)
+                db.session.add(new_report_1130)
+                new_report_1700 = Hour_report(user_id=user_id, time=time_1700)
+                db.session.add(new_report_1700)
                 db.session.commit()
-            flash("Báo cáo thành công!", category='success')
 
-        if form_id == 'form-1700':
-            user_id = request.form.get('user_id')
-            report = Hour_report.query.filter_by(user_id=user_id, time=time_1700).first()
-            if report:
-                report.cost = request.form.get(f'cost')
-                report.phoneNumber = request.form.get(f'phoneNumber')
-                report.revenue = request.form.get(f'revenue')
-                db.session.commit()
-            flash("Báo cáo thành công!", category='success')
+        if request.method == 'POST':
+            form_id = request.form.get('form_id')
 
-    staffs = User.query.all()
-    staff_list = []
-    for staff in staffs:
-        staff_data = {
-            'id': staff.id,
-            'name': staff.name
-        }
-        staff_list.append(staff_data)
-    
-    reports = Hour_report.query.filter(or_(Hour_report.time == time_1130, Hour_report.time == time_1700)).all()
-    report_list = []
+            if form_id == 'form-1130':
+                user_id = request.form.get('user_id')
+                report = Hour_report.query.filter_by(user_id=user_id, time=time_1130).first()
+                if report:
+                    report.cost = request.form.get(f'cost')
+                    report.phoneNumber = request.form.get(f'phoneNumber')
+                    report.revenue = request.form.get(f'revenue')
+                    db.session.commit()
+                flash("Báo cáo thành công!", category='success')
 
-    for report in reports:
-        report_data = {
-            'user_id': report.user_id,
-            'time': '1130' if report.time==time_1130 else '1700',
-            'cost': f'{report.cost:,}',
-            'phoneNumber': f'{report.phoneNumber:,}',
-            'costPerPhone': f'{int(round(report.cost/report.phoneNumber, 0) if report.phoneNumber != 0 else 0):,}',
-            'revenue': f'{report.revenue:,}',
-            'cpqc': f'{round(report.cost/report.revenue * 100 if report.revenue != 0 else 0, 2):,}',
-            'roat': f'{round(report.revenue/report.cost if report.cost != 0 else 0, 2):,}'
-        }
-        report_list.append(report_data)
+            if form_id == 'form-1700':
+                user_id = request.form.get('user_id')
+                report = Hour_report.query.filter_by(user_id=user_id, time=time_1700).first()
+                if report:
+                    report.cost = request.form.get(f'cost')
+                    report.phoneNumber = request.form.get(f'phoneNumber')
+                    report.revenue = request.form.get(f'revenue')
+                    db.session.commit()
+                flash("Báo cáo thành công!", category='success')
 
-    return render_template('bao-cao-gio.html', user=current_user, staff_list=staff_list, report_list=report_list, ddate=ddate, render_table=render_table)
+        staffs = User.query.all()
+        staff_list = []
+        for staff in staffs:
+            staff_data = {
+                'id': staff.id,
+                'name': staff.name
+            }
+            staff_list.append(staff_data)
+        
+        reports = Hour_report.query.filter(or_(Hour_report.time == time_1130, Hour_report.time == time_1700)).all()
+        report_list = []
+
+        for report in reports:
+            report_data = {
+                'user_id': report.user_id,
+                'time': '1130' if report.time==time_1130 else '1700',
+                'cost': f'{report.cost:,}',
+                'phoneNumber': f'{report.phoneNumber:,}',
+                'costPerPhone': f'{int(round(report.cost/report.phoneNumber, 0) if report.phoneNumber != 0 else 0):,}',
+                'revenue': f'{report.revenue:,}',
+                'cpqc': f'{round(report.cost/report.revenue * 100 if report.revenue != 0 else 0, 2):,}',
+                'roat': f'{round(report.revenue/report.cost if report.cost != 0 else 0, 2):,}'
+            }
+            report_list.append(report_data)
+
+        return render_template('bao-cao-gio.html', user=current_user, staff_list=staff_list, report_list=report_list, ddate=ddate, render_table=render_table)
+    except:
+        return render_template('404.html')
 
 
 @views.route('/bao-cao-bai-test', methods=['GET'])
@@ -258,31 +260,56 @@ def bcbtest():
 @views.route('/bao-cao-bai-test/<int:yb>-<int:mb>-<int:dbb>-<int:y>-<int:m>-<int:d>', methods=['GET', 'POST'])
 @login_required
 def bao_cao_bai_test(yb, mb, dbb, y, m, d):
+    # try:
     date1 = datetime.strptime(f'{yb}-{mb}-{dbb}', '%Y-%m-%d').date()
     date2 = datetime.strptime(f'{y}-{m}-{d}', '%Y-%m-%d').date()
 
     if request.method == 'POST':
         form_id = request.form.get('form_id')
 
-        if form_id == 'form-1130':
-            # user_id = request.form.get('user_id')
-            # report = Hour_report.query.filter_by(user_id=user_id, time=time_1130).first()
-            # if report:
-            #     report.cost = request.form.get(f'cost')
-            #     report.phoneNumber = request.form.get(f'phoneNumber')
-            #     report.revenue = request.form.get(f'revenue')
-            #     db.session.commit()
-            flash("Báo cáo thành công!", category='success')
+        if form_id == 'form-date-filter':
+            dateInput1 = request.form.get('dateInput1')
+            dateInput2 = request.form.get('dateInput2')
 
-        if form_id == 'form-1700':
-            # user_id = request.form.get('user_id')
-            # report = Hour_report.query.filter_by(user_id=user_id, time=time_1700).first()
-            # if report:
-            #     report.cost = request.form.get(f'cost')
-            #     report.phoneNumber = request.form.get(f'phoneNumber')
-            #     report.revenue = request.form.get(f'revenue')
-            #     db.session.commit()
-            flash("Báo cáo thành công!", category='success')
+            date1 = datetime.strptime(dateInput1, '%Y-%m-%d').date()
+            date2 = datetime.strptime(dateInput2, '%Y-%m-%d').date()
+
+            return redirect(url_for(f'views.bao_cao_bai_test', yb=date1.year, mb=date1.month, dbb=date1.day, y=date2.year, m=date2.month, d=date2.day))
+
+        if form_id == 'form-add':
+            user_id = request.form.get('user_id')
+            link = request.form.get('link')
+            content = request.form.get('content')
+            type = request.form.get('type')
+            budget = request.form.get('budget') if request.form.get('budget') != '' else 0
+            cpm = request.form.get('cpm') if request.form.get('cpm') != '' else 0
+            cpc = request.form.get('cpc') if request.form.get('cpc') != '' else 0
+            ctr = request.form.get('ctr') if request.form.get('ctr') != '' else 0
+            cpa = request.form.get('cpa') if request.form.get('cpa') != '' else 0
+            order = request.form.get('order') if request.form.get('order') != '' else 0
+            review = request.form.get('review')
+            new_post = Post(user_id=user_id, time=date.today(), link=link, content=content,
+                            type=type, budget=budget, cpm=cpm, cpc=cpc, ctr=ctr, cpa=cpa, order=order, review=review)
+            db.session.add(new_post)
+            db.session.commit()
+            flash("Thêm bài test mới thành công!", category='success')
+
+        if form_id == 'form-edit':
+            id = request.form.get('edit-post_id')
+            post = Post.query.get(id)
+            if post:
+                post.link = request.form.get('edit-link')
+                post.content = request.form.get('edit-content')
+                post.type = request.form.get('edit-type')
+                post.budget = request.form.get('edit-budget') if request.form.get('budget') != '' else 0
+                post.cpm = request.form.get('edit-cpm') if request.form.get('cpm') != '' else 0
+                post.cpc = request.form.get('edit-cpc') if request.form.get('cpc') != '' else 0
+                post.ctr = request.form.get('edit-ctr') if request.form.get('ctr') != '' else 0
+                post.cpa = request.form.get('edit-cpa') if request.form.get('cpa') != '' else 0
+                post.order = request.form.get('edit-order') if request.form.get('order') != '' else 0
+                post.review = request.form.get('edit-review')
+                db.session.commit()
+                flash("Chỉnh sửa bài test thành công!", category='success')
 
     staffs = User.query.all()
     staff_list = []
@@ -298,20 +325,34 @@ def bao_cao_bai_test(yb, mb, dbb, y, m, d):
 
     for post in posts:
         post_data = {
+            'id': post.id,
             'user_id': post.user_id,
+            'time': post.time.strftime("%d/%m/%Y"),
             'link': post.link,
             'type': post.type,
             'content': post.content,
-            'budget': post.budget,
-            'cpm': post.cpm,
-            'cpc': post.cpc,
-            'ctr': post.ctr,
-            'cpa': post.cpa,
-            'cpPerCpa': post.cpPerCpa,
-            'order': post.order,
-            'cpPerOrder': post.cpPerOrder,
+            'budget': f'{post.budget:,}',
+            'cpm': f'{post.cpm:,}',
+            'cpc': f'{post.cpc:,}',
+            'ctr': round(post.ctr, 2),
+            'cpa': f'{post.cpa:,}',
+            'cpPerCpa': f'{int(round(post.budget/post.cpa, 0) if post.cpa != 0 else 0):,}',
+            'order': f'{post.order:,}',
+            'cpPerOrder': f'{int(round(post.budget/post.order, 0) if post.order != 0 else 0):,}',
             'review': post.review,
         }
         post_list.append(post_data)
 
     return render_template('bao-cao-bai-test.html', user=current_user, staff_list=staff_list, post_list=post_list, date1=date1, date2=date2)
+    # except:
+    #     return render_template('404.html')
+
+
+@views.route('/post/delete/<int:id>', methods=['POST'])
+@login_required
+def delete_post(id):
+    post = Post.query.get(id)
+    if post:
+        db.session.delete(post)
+        db.session.commit()
+    return redirect(url_for('views.bao-cao-bai-test'), user=current_user)
